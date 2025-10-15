47b50aed66eb8b0e43743734c765616ab1e344bd
Aleks-Ti <mti61ro@yandex.ru>
2025-10-15 20:31:44 +0300

    довел до ума clap

--- src/lib.rs
+++ src/lib.rs
@@ -1,66 +1,32 @@
 use std::{fs::File, io::Read};
 
+use clap::ValueEnum;
 pub mod parser;
 
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
-
-#[derive(clap::Parser)]
-struct Cli {
-    #[arg(short, long, value_enum)]
-    in_format: Format,
-    #[arg(short, long, value_enum)]
-    out_format: Format,
-    #[arg(short, long)]
-    input: Option<String>,
-    #[arg(short, long)]
-    output: Option<String>,
-}
-
-#[derive(clap::ValueEnum, Clone)]
+#[derive(ValueEnum, Clone, Debug)]
 enum Format {
     Csv,
     Mt940,
     Camt053,
 }
 
-
 struct Converter;
 
-
 impl Converter {
-    pub fn to_csv() {
-        
-    }
-    pub fn to_mt940() {
-        
-    }
-    pub fn to_camt053(&mut self, file: &File) {
-        let data = file.read(file).unwrap();
-    }
-    pub fn from_csv_to_camt053() {
-        
-    }
-    pub fn from_csv_to_mt940() {
-        
-    }
-    pub fn from_camt053_to_csv() {
-        
-    }
-    pub fn from_camt053_to_mt940() {
-        
-    }
-    pub fn from_mt940_to_csv() {
-        
-    }
-    pub fn from_mt940_to_camt053() {
-        
-    }
-
+    pub fn to_csv() {}
+    pub fn to_mt940() {}
+    pub fn to_camt053(&mut self, file: &File) {}
+    pub fn from_csv_to_camt053() {}
+    pub fn from_csv_to_mt940() {}
+    pub fn from_camt053_to_csv() {}
+    pub fn from_camt053_to_mt940() {}
+    pub fn from_mt940_to_csv() {}
+    pub fn from_mt940_to_camt053() {}
 }
-
 
 #[cfg(test)]
 mod tests {
