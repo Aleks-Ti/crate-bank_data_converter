09b1934400e6488ed05850d6d104d4152c33612f
Aleks-Ti <mti61ro@yandex.ru>
2025-10-19 03:29:04 +0300

    почистил uploader - задублировался

--- upload/src/lib.rs
+++ upload/src/lib.rs
@@ -4,11 +4,9 @@ pub trait Uploader {
 }
 
 #[derive(Debug)]
-pub struct FileSystemOutput;
-#[derive(Debug)]
-pub struct IoOutput;
+pub struct FileUpload;
 
-impl Uploader for FileSystemOutput {
+impl Uploader for FileUpload {
     fn upload(data: &[u8], path: Option<&str>) -> io::Result<()> {
         if let Some(filename) = path {
             std::fs::write(filename, data)?;
@@ -17,41 +15,35 @@ impl Uploader for FileSystemOutput {
             io::stdout().write_all(data)?;
             Ok(())
         }
-    }
-}
-
-impl Uploader for IoOutput {
-    fn upload(data: &[u8], path: Option<&str>) -> io::Result<()> {
-        if let Some(filename) = path {
-            std::fs::write(filename, data)?;
-        } else {
-            io::stdout().write_all(data)?;
-        }
-        Ok(())
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::fs;
     use super::*;
+    use std::fs;
 
     #[test]
     fn file_system_output() {
         let data = "tests".as_bytes();
         let path = "test_file.csv";
-        let _ = FileSystemOutput::upload(data, Some(path));
+        let _ = FileUpload::upload(data, Some(path));
         assert!(std::path::Path::new("test_file.csv").exists());
         let _ = fs::remove_file(path);
     }
 
     #[test]
-    fn stdout_output() {
+    fn file_system_output_temp() {
         let data = "tests".as_bytes();
         let temp_file = "test_output.tmp";
-        let _ = FileSystemOutput::upload(data, Some(temp_file)).unwrap();
+        let _ = FileUpload::upload(data, Some(temp_file)).unwrap();
         let content = fs::read_to_string(temp_file).unwrap();
         assert_eq!(content, "tests");
         let _ = fs::remove_file(temp_file);
+    }
+    #[test]
+    fn io_output_stdout() {
+        let data = "TEST STDOUT\n".as_bytes();
+        FileUpload::upload(data, None).unwrap();
     }
 }
